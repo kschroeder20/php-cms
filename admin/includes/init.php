21c0897185cf796b314a5c42acc4758dbbbc237f
Kevin Schroeder <schroederkevin94@gmail.com>
2020-04-24 08:58:04 -0400

    Comment functionality WIP

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -10,9 +10,10 @@ require_once('new_config.php');
 require_once('database.php');
 require_once('db_object.php');
 require_once('user.php');
-require_once('photo.php');
+require_once(__DIR__ . DS . 'photo.php');
 require_once('functions.php');
 require_once('session.php');
+require_once('comment.php');
 
 
 
