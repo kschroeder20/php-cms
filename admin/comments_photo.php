583643d54e8f737e8c3a839f69aad5356a95147a
Kevin Schroeder <schroederkevin94@gmail.com>
2020-04-27 08:58:43 -0400

    Build Out Comment Section * Build out link to show comment for each photo * Create the ability to delete comments * Begining work on CMS dashboard

--- admin/comments_photo.php
+++ admin/comments_photo.php
@@ -4,7 +4,14 @@
 
 <?php 
 
-$comments = Comment::find_all();
+if(empty($_GET['id'])) {
+
+  redirect('photos.php');
+
+}
+
+$comments = Comment::find_the_comments($_GET['id']);
+
 
 ?>
 
@@ -27,15 +34,10 @@ $comments = Comment::find_all();
     <!-- Page Heading -->
     <div class="row">
       <div class="col-lg-12">
-        <h1 class="page-header">
-          Comments
-        </h1>
-
-        <a href="add_user.php" class="btn btn-primary">Add Comment</a>
-
+        <h1 class="page-header">Comments</h1>
 
         <div class="col-md-12">
-
+          
           <table class="table table-hover">
             <thead>
               <tr>
@@ -51,7 +53,7 @@ $comments = Comment::find_all();
                 <td>
                   <?php echo $comment->author; ?>
                   <div class="action_links">
-                    <a href="delete_comment.php?id=<?php echo $comment->id ?>">Delete</a>
+                    <a href="delete_comment_photo.php?id=<?php echo $comment->id ?>">Delete</a>
                   </div>
                 </td>
                 <td><?php echo $comment->body; ?></td>
--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,59 +1,117 @@
 <div class="container-fluid">
 
-<!-- Page Heading -->
-<div class="row">
+  <!-- Page Heading -->
+  <div class="row">
     <div class="col-lg-12">
-        <h1 class="page-header">
-            Admin
-            <small>Subheading</small>
-        </h1>
+      <h1 class="page-header">
+        Admin
+        <small>Subheading</small>
+      </h1>
 
-        <?php 
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-users fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">4</div>
+                  <div>New Views</div>
+                </div>
+              </div>
+            </div>
+            <a href="#">
+              <div class="panel-footer">
+                <div>Page View from Gallery</div>
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
 
-        // $photo = new Photo();
-
-        // $photo->title = "The best photo ever";
-        // $photo->size = 20;
-
-        // $photo->create();
-
-        // $photo = Photo::find_by_id(6);
-        // echo $photo->filename;
-        // $user->last_name = "Williams";
-
-
-        // $user->update();
-
-        // $user = User::find_user_by_id(3);
-
-        // $user->delete();
-
-        // $photos = Photo::find_all();
-
-        // foreach ($photos as $photo) {
-
-        //     echo $photo->title;
-
-        // }
-
-        // echo INCLUDES_PATH;
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-green">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-photo fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">7</div>
+                  <div>Photos</div>
+                </div>
+              </div>
+            </div>
+            <a href="#">
+              <div class="panel-footer">
+                <span class="pull-left">Total Photos in Gallery</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
 
 
-        
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-yellow">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">8
 
-        ?>
+                  </div>
 
-        <ol class="breadcrumb">
-            <li>
-                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-            </li>
-            <li class="active">
-                <i class="fa fa-file"></i> Blank Page
-            </li>
-        </ol>
+                  <div>Users</div>
+                </div>
+              </div>
+            </div>
+            <a href="#">
+              <div class="panel-footer">
+                <span class="pull-left">Total Users</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-red">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-support fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">8</div>
+                  <div>Comments</div>
+                </div>
+              </div>
+            </div>
+            <a href="#">
+              <div class="panel-footer">
+                <span class="pull-left">Total Comments</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+
+      </div>
+      <!--First Row-->
     </div>
-</div>
-<!-- /.row -->
+  </div>
+  <!-- /.row -->
 
 </div>
 <!-- /.container-fluid -->
