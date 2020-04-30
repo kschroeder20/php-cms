ed9fc6cee2443fb2c2d979bc90a31e843d7243c5
Kevin Schroeder <schroederkevin94@gmail.com>
2020-04-30 11:24:29 -0400

    Add Database Information

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -2,7 +2,7 @@
 
 // Database Connection Constants
 
-define('DB_HOST','zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+define('DB_HOST','zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('DB_USER','p4os6ig0mlfna42w');
 define('DB_PASS','e48wzbh41k8u5qkf');
 define('DB_NAME','bc3b6hgbosmpaj0s');
